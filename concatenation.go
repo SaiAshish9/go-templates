4b1c623bf57e757b7ddc42e13d73d61302395936
saiashish9 <saiashish3760@gmail.com>
2021-01-20 23:14:24 +0530

    initial commit

--- concatenation.go
+++ concatenation.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Go")
+	name := os.Args[1]
+	fmt.Println(os.Args[0])
+	fmt.Println(os.Args[1])
+	str := fmt.Sprint(`
+	<!DOCTYPE html>
+	<html lang="en">
+	<head>
+	<meta charset="UTF-8" >
+	<title>Conatenation</title>
+	</head>
+	<body>
+	<h1>` +
+		name +
+		`</h1>
+	</body>
+	</html>
+	`)
+
+	nf, err := os.Create("index.html")
+	if err != nil {
+		log.Fatal("error")
+	}
+
+	defer nf.Close()
+	io.Copy(nf, strings.NewReader(str))
 }
