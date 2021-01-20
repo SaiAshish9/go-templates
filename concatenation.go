60e9defb436fc65a80f804dcbb383c02fbbed3d4
saiashish9 <saiashish3760@gmail.com>
2021-01-21 01:09:20 +0530

    templates updated

--- concatenation.go
+++ concatenation.go
@@ -35,3 +35,10 @@ func main() {
 	defer nf.Close()
 	io.Copy(nf, strings.NewReader(str))
 }
+
+// A defer statement defers the execution of
+// a function
+// until the surrounding function returns.
+// The deferred call's arguments are evaluated
+// immediately, but the function call is not
+// executed until the surrounding function returns.
