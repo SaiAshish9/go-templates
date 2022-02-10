57fb32318ddd7bcc9febd0fa22ca918902b3af56
SaiAshish9 <43849911+SaiAshish9@users.noreply.github.com>
2022-02-10 16:10:34 +0530

    k

--- PackageTextTemplate/temp.go
+++ PackageTextTemplate/temp.go
@@ -1,32 +1,24 @@
 package main
-
 import (
 	"log"
 	"os"
 	"text/template"
 )
-
 var tpl *template.Template
-
 type a1 struct {
 	A string
 	B int
 }
-
 type b1 struct {
 	C []a1
 }
-
 func init() {
 	tpl = template.Must(template.ParseGlob("*"))
 	// templates/*.txt
 	// templates/*
 }
-
 func main() {
-
 	// every type implements the empty interface
-
 	err := tpl.Execute(os.Stdout, nil)
 	var data interface{}
 	data = []string{"a", "b", "c"}
@@ -37,21 +29,15 @@ func main() {
 		C []{data,data}
 	}
 	_ = data
-
-
 	var fm = template.FuncMap(
 		"us": strings.ToUpper,
 		"ft": firstThree
 	)
-
 	// err = tpl.ExecuteTemplate(os.Stdout, "a.txt", nil)
 	// err = tpl.ExecuteTemplate(os.Stdout, "tpl.gohtml", 42)
 	// err = tpl.ExecuteTemplate(os.Stdout, "tpl1.gohtml", `test`)
-
 	err = tpl.ExecuteTemplate(os.Stdout, "tpl1.gohtml", data)
-
 	if err != nil {
 		log.Fatalln(err)
 	}
-
 }
